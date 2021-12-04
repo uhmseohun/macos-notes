804c3c480d0e03ff7309d449bac5854bb55cf206
엄서훈 <uhmtotow@gmail.com>
2021-12-04 10:21:56 +0900

    body 백그라운드 컬러 적용

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -5,6 +5,13 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
   }
+
+  body {
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+    background: #E3E4E5;
+  }
 `;
 
 export default GlobalStyle;
