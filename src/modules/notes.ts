29818e86f0620f9ace5758a58213451ca951caff
uhmseohun <uhmtotow@gmail.com>
2021-12-05 21:20:08 +0900

    Redux 치명적인 실수 제거

--- src/modules/notes.ts
+++ src/modules/notes.ts
@@ -1,19 +1,8 @@
 import { Note } from "../types";
 
-const GET_NOTES = "notes/GET_NOTES" as const;
-const GET_NOTE = "notes/GET_NOTE" as const;
 const REMOVE_NOTE = "notes/REMOVE_NOTE" as const;
 const UPDATE_NOTE = "notes/UPDATE_NOTE" as const;
 const CREATE_NOTE = "notes/CREATE_NOTE" as const;
-
-export const getNotes = () => ({
-  type: GET_NOTES,
-});
-
-export const getNote = (id: number) => ({
-  type: GET_NOTE,
-  id,
-});
 
 export const removeNote = (id: number) => ({
   type: REMOVE_NOTE,
@@ -32,8 +21,6 @@ export const createNote = (payload: Note) => ({
 });
 
 type Action =
-  | ReturnType<typeof getNotes>
-  | ReturnType<typeof getNote>
   | ReturnType<typeof removeNote>
   | ReturnType<typeof updateNote>
   | ReturnType<typeof createNote>;
@@ -46,10 +33,6 @@ const initialState: State = JSON.parse(
 
 const reducer = (state: State = initialState, action: Action) => {
   switch (action.type) {
-    case GET_NOTES:
-      return state;
-    case GET_NOTE:
-      return state.find((note) => note.id === action.id);
     case REMOVE_NOTE:
       return state.filter((note) => note.id !== action.id);
     case UPDATE_NOTE:
